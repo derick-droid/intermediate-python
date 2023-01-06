d28d1d2216e27726030c94cf1247e953333bfcfd
derick-droid <derrickokinda9@gmail.com>
2023-01-06 22:10:38 +0300

    q3

---
+++ if/exercise/no3/q3.py
@@ -0,0 +1,21 @@
+"""
+. Ask the user to enter a temperature in Celsius. The program should print a message based
+on the temperature:
+• If the temperature is less than -273.15, print that the temperature is invalid because it is
+below absolute zero.
+• If it is exactly -273.15, print that the temperature is absolute 0.
+• If the temperature is between -273.15 and 0, print that the temperature is below freezing.
+• If it is 0, print that the temperature is at the freezing point.
+• If it is between 0 and 100, print that the temperature is in the normal range.
+• If it is 100, print that the temperature is at the boiling point.
+• If it is above 100, print that the temperature is above the boiling point
+"""
+
+
+def three():
+    temp = int(input("Enter your temperature in celsius: "))
+    if temp < -273.15:
+        print("temperature is invalid because it is below absolute zero")
+    elif temp == -273.15:
+        print("temperature is absolute 0.")
+    elif temp 
