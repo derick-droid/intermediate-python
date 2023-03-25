a941a6578fe0ecd0cf8459ffd2f207c610478a23
derick-droid <derrickokinda9@gmail.com>
2023-03-25 03:01:22 +0300

    counting example 2

--- Miscellaneous Topics I/intro/main.py
+++ Miscellaneous Topics I/intro/main.py
@@ -16,3 +16,25 @@ def counting():
 
 counting()
 
+"""
+Example 2 This modification of the previous example counts how many of the numbers the user
+enters are  greater than 10 and also how many are equal to 0. To count two things we use two count
+variables.
+"""
+
+
+def greater():
+    count1 = 0
+    count2 = 0
+    for j in range(5):
+        num1 = int(input("Enter number: "))
+        if num1 > 10:
+            count1 = count1 + 1
+        elif num1 < 10:
+            count2 = count2 + 1
+        else:
+            print("Invalid entry")
+    print(f"there are {count1} numbers  greater than 10 and {count2} numbers less than 10")
+
+
+greater()
