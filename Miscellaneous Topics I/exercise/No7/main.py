6b667732ee3d72c88f671f631b55cc1888b283e4
derick-droid <derrickokinda9@gmail.com>
2023-04-08 15:24:25 +0300

    square free number question

--- Miscellaneous Topics I/exercise/No7/main.py
+++ Miscellaneous Topics I/exercise/No7/main.py
@@ -7,4 +7,26 @@ divisible by 9, which is a perfect square. Write a program that asks the user fo
 tells them if it is squarefree or not.
 
 """
-def squarefree():
+
+import math
+
+
+def seven():
+    flag = 0
+    num = int(input("Enter a number: "))
+    for i in range(2, num + 1):
+        square_root = math.sqrt(i)
+        if square_root == int(square_root):
+            flag = 1
+
+    if flag == 1:
+        print(f"{num} is a square free number ")
+    else:
+        print(f"{num} is not a square free number")
+
+
+seven()
+
+
+
+
