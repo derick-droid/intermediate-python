e053c3486b55897fd897d441744ab714ad69b393
derick-droid <derrickokinda9@gmail.com>
2023-04-09 13:29:47 +0300

    number 10

--- Miscellaneous Topics I/exercise/No10/main.py
+++ Miscellaneous Topics I/exercise/No10/main.py
@@ -10,11 +10,11 @@ a message warning the user that a value over 100 has been entered.
 
 
 def ten():
+
     summ = 0
     flg = 0
     average = 0
-    highest_score = 0
-    lowest_score = 0
+
     list_1 = []
     for i in range(10):
         num = int(input("Enter scores: "))
@@ -32,7 +32,33 @@ def ten():
         print("A value greater than 100 has been entered")
 
 #     finding the highest and lowest value
-   print()
+    highest_score = list_1[0]
+    lowest_score = list_1[0]
+    for number in list_1:
+        if number > highest_score:
+            highest_score = number
+    print(f"{highest_score} is the largest score")
+
+    for n in list_1:
+        if n < lowest_score:
+            lowest_score = n
+
+    print(f"{lowest_score} is the lowest score")
+# finding second largest
+    list_sorted = sorted(list_1)
+    print(f"{list_sorted[8]} is the second largest number")
+    print(list_1)
+
+#     Drop the two lowest scores and print out the average of the rest of them
+
+    del list_sorted[0:2]
+    sum_1 = 0
+    new_average = 0
+    for value in list_sorted:
+        sum_1 = sum_1 + value
+
+    new_average = sum_1 / len(list_sorted)
+    print(f"the average number of remaining score is {new_average}")
 
 
 ten()
