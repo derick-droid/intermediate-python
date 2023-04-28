ed5f0b9d2210cfbfe4a5151706eb8e70206e4bf2
derick-droid <derrickokinda9@gmail.com>
2023-04-28 10:41:27 +0300

    methods

--- string/methods/main.py
+++ string/methods/main.py
@@ -1,3 +1,25 @@
 """
 string methods
 """
+s = "coming home"
+
+print(s.upper())
+print(s.index("c"))
+print(s.count(" "))
+
+"""
+isalpha
+"""
+word = input("Enter name: ")
+if word[0].isalpha():
+    print("The word you have entered starts with a letter")
+if not word.isalpha():
+    print("The word you have entered is a non letter")
+
+"""
+index
+"""
+
+if "c" in s:
+    print(s.index("c"))
+
