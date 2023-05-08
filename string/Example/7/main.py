0c51a6adc3fa10b78ae299f4b4782f54a7c16ae6
derick-droid <derrickokinda9@gmail.com>
2023-05-08 20:12:22 +0300

    quiz 1

---
+++ string/Example/7/main.py
@@ -0,0 +1,6 @@
+"""
+A simple and very old method of sending secret messages is the substitution cipher.
+Basically, each letter of the alphabet gets replaced by another letter of the alphabet, say every a gets
+replaced with an x, and every b gets replaced by a z, etc. Write a program to implement this.
+"""
+
---
+++ string/exercise/no 1/1a/1a.py
@@ -0,0 +1,9 @@
+"""
+Write a program that asks the user to enter a string. The program should then print the
+following:
+(a) The total number of characters in the string
+
+"""
+string = input("Enter a string: ")
+print(len(string))
+
