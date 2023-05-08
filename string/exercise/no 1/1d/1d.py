693d54cfb320f36c7aa2b074c6947e22a98aeb15
derick-droid <derrickokinda9@gmail.com>
2023-05-08 20:20:57 +0300

    Quiz 1d

---
+++ string/exercise/no 1/1d/1d.py
@@ -0,0 +1,9 @@
+"""
+Write a program that asks the user to enter a string. The program should then print the
+following:
+(d) The first three characters of the string
+"""
+string = input("Enter a string: ")
+print(string[0])
+print(string[1])
+print(string[2])
