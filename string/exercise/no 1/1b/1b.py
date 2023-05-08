c48bf6cb1782730b10adfcaaf6b67e7c5c71729a
derick-droid <derrickokinda9@gmail.com>
2023-05-08 20:15:30 +0300

    quiz 1b

---
+++ string/exercise/no 1/1b/1b.py
@@ -0,0 +1,8 @@
+"""
+Write a program that asks the user to enter a string. The program should then print the
+following:
+(b) The string repeated 10 times
+
+"""
+string = input("Enter a string: ")
+print(string * 10, end=" ")
