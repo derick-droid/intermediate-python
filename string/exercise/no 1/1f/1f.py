192504f1d0de183ffe383e105a184ee926c0332a
derick-droid <derrickokinda9@gmail.com>
2023-05-08 20:29:07 +0300

    Quiz 1f

---
+++ string/exercise/no 1/1f/1f.py
@@ -0,0 +1,8 @@
+"""
+Write a program that asks the user to enter a string. The program should then print the
+following:
+(f) The string backwards
+
+"""
+string = input("Enter a string: ")
+print(string[  : : -1])
