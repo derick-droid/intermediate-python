9baa5f20450d99cbdbc1913abd775ccf4963064c
derick-droid <derrickokinda9@gmail.com>
2023-05-08 20:23:10 +0300

    Quiz 1e

---
+++ string/exercise/no 1/1e/1e.py
@@ -0,0 +1,10 @@
+"""
+Write a program that asks the user to enter a string. The program should then print the
+following:
+(e) The last three characters of the string
+
+"""
+string = input("Enter a string: ")
+print(string[-1])
+print(string[-2])
+print(string[-3])
