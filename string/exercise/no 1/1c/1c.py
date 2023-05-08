d54c0efafda1cfd7845cb12017415f7968d0be23
derick-droid <derrickokinda9@gmail.com>
2023-05-08 20:18:27 +0300

    quiz 1c

---
+++ string/exercise/no 1/1c/1c.py
@@ -0,0 +1,8 @@
+"""
+Write a program that asks the user to enter a string. The program should then print the
+following:
+(c) The first character of the string (remember that string indices start at 0)
+
+"""
+string = input("Enter a string: ")
+print(string[0])
