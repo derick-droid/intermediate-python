eeb2fb03e8b8315b3b98c0c67972106fd3540a2c
derick-droid <derrickokinda9@gmail.com>
2023-05-13 20:34:57 +0300

    quiz 1h

---
+++ string/exercise/no 1/i/main.py
@@ -0,0 +1,16 @@
+"""
+. Write a program that asks the user to enter a string. The program should then print the
+following:
+
+(i) The string in all caps
+
+"""
+
+
+def the_i():
+    word = input("Enter a word: ")
+    print(word.upper())
+
+
+the_i()
+
