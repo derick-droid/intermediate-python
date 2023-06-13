1b44fdc87154a4294f04ad196a7b648b0c308458
derick-droid <derrickokinda9@gmail.com>
2023-06-13 12:08:55 +0300

    complete Quiz 7

---
+++ string/exercise/no 7/main.py
@@ -0,0 +1,14 @@
+"""
+ Write a program that asks the user to enter a word and determines whether the word is a
+palindrome or not. A palindrome is a word that reads the same backwards as forwards.
+"""
+
+
+def seven():
+    word = input("Enter a word: ").lower()
+    if word == word[: : -1]:
+        print("The word is palindrome")
+    else:
+        print("The word you have entered is not palindrome")
+
+seven()
