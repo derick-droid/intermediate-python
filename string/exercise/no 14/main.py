19f4b6209e14d403374b38284e3db7810f7e829a
derick-droid <derrickokinda9@gmail.com>
2023-06-16 19:32:40 +0300

    14

---
+++ string/exercise/no 14/main.py
@@ -0,0 +1,13 @@
+"""
+Write a program that asks the user to enter their name in lowercase and then capitalizes the
+first letter of each word of their name.
+
+"""
+
+
+def fourteen():
+    name = input("Enter your name in lowercase: ")
+    print(name.title())
+
+
+fourteen()
