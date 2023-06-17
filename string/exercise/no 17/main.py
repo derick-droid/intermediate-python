188a1eaacfb787187f270df8c6c473a8b8b5efe9
derick-droid <derrickokinda9@gmail.com>
2023-06-17 12:53:40 +0300

    17

--- string/exercise/no 17/main.py
+++ string/exercise/no 17/main.py
@@ -12,13 +12,23 @@ zabcdefghijklmnopqrstuvwxy
 
 
 def seventeen():
-    alphabet = " abcdefghijklmnopqrstuvwxyz "
-    for m in range(26):
+    """
+    This program uses a nested loop to generate each line of letters. The outer loop iterates from 0 to 25, representing the index of each letter in the alphabet. The inner loop iterates from 0 to 25 as well, representing the position of each letter in the line.
+
+Inside the inner loop, we calculate the index of the letter by adding the outer loop index (i) and the inner loop index (j), modulo 26 (the number of letters in the alphabet). We convert the index back to a character using chr() and add it to the line string.
+    :return:
+    """
+    for i in range(26):
+        line = ""
         for j in range(26):
-            print(alphabet[(m + j) % 26], end='')
-        print()
+            line += chr((i + j) % 26 + ord('a'))
+        print(line)
+
+
+
 
 
 seventeen()
+
 
 
