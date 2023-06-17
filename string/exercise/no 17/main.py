7e157d2cda774771bae7edf946cc8f0f221f3ab6
derick-droid <derrickokinda9@gmail.com>
2023-06-17 12:18:05 +0300

    17

---
+++ string/exercise/no 17/main.py
@@ -0,0 +1,24 @@
+"""
+Write a program that generates the 26-line block of letters partially shown below. Use a loop
+containing one or two print statements.
+abcdefghijklmnopqrstuvwxyz
+bcdefghijklmnopqrstuvwxyza
+cdefghijklmnopqrstuvwxyzab
+...
+yzabcdefghijklmnopqrstuvwx
+zabcdefghijklmnopqrstuvwxy
+
+"""
+
+
+def seventeen():
+    alphabet = " abcdefghijklmnopqrstuvwxyz "
+    for m in range(26):
+        for j in range(26):
+            print(alphabet[(m + j) % 26], end='')
+        print()
+
+
+seventeen()
+
+
