5f24ce17b45f183aa0ca9e2cf260956408bfee1c
derick-droid <derrickokinda9@gmail.com>
2023-06-20 19:28:53 +0300

    19

--- string/exercise/no 19/main.py
+++ string/exercise/no 19/main.py
@@ -8,6 +8,9 @@ enters 1000000, the output should be 1,000,000.
 
 def nineteen():
     number = input("Enter a large integer: ")
+    if len(number) >= 4:
+        res = '{:,}'.format(int(number))
+        print(res)
 
 
 nineteen()
