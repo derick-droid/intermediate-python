e6bfaedab1c1ec01d14e9198c2b1517d012f0327
derick-droid <derrickokinda9@gmail.com>
2023-06-23 21:34:21 +0300

    20

---
+++ string/exercise/no 20/main.py
@@ -0,0 +1,17 @@
+"""
+Write a program that converts a time from one time zone to another. The user enters the time
+in the usual American way, such as 3:48pm or 11:26am. The first time zone the user enters
+is that of the original time and the second is the desired time zone. The possible time zones
+are Eastern, Central, Mountain, or Pacific.
+Time: 11:48pm
+Starting zone: Pacific
+Ending zone: Eastern
+2:48am
+
+"""
+
+def twenty():
+    Time = input("Enter time: ")
+    time_zone: input("Enter the time zone: ")
+    next_time_zone = input("Enter your desired time zone: ")
+    
