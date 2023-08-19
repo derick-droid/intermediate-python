e4da080f122d1b9f32e0372085be04a1ad5b9636
derick-droid <derrickokinda9@gmail.com>
2023-08-19 11:26:57 +0300

    list

---
+++ List/basic/main.py
@@ -0,0 +1,15 @@
+"""
+Input We can use eval(input()) to allow the user to enter a list. Here is an example:
+"""
+
+l = input("Enter a list: ")
+print(l)
+
+"""
+Data types
+valid list:
+Lists can contain all kinds of things, even other lists. For example, the following is a
+"""
+
+l2 = input("Enter a list: ")
+print(l2)
