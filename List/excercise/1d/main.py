af36f277dd18d5e6dcc64590284912167ca445b5
derick-droid <derrickokinda9@gmail.com>
2023-09-07 05:16:31 +0300

    1d

---
+++ List/excercise/1d/main.py
@@ -0,0 +1,33 @@
+"""
+Write a program that asks the user to enter a list of integers. Do the following:
+(a) Print the total number of items in the list.
+(b) Print the last item in the list.
+(c) Print the list in reverse order.
+(d) Print Yes if the list contains a 5 and No otherwise.
+
+"""
+
+
+def enter():
+
+    list = []
+    available_five = 0
+    for num in range(5):
+        number = input("Enter integer: ")
+        list.append(number)
+
+    print(list)
+
+    for number in list:
+        if number == 5:
+            print(number)
+            available_five = available_five + 1
+
+    if available_five == 0:
+        print("NO")
+    elif available_five >= 1:
+        print("YES")
+    print(available_five)
+
+
+enter()
