75112c82b5b10bf91a1cbcd7487a32283b582d0c
derick-droid <derrickokinda9@gmail.com>
2023-09-15 16:22:47 +0300

    3a

---
+++ List/excercise/3a/main.py
@@ -0,0 +1,16 @@
+"""
+Start with the list [8,9,10]. Do the following:
+(a) Set the second entry (index 1) to 17
+"""
+
+
+def three():
+    list_value = [8, 9, 10]
+
+    list_value.insert(1, 17)
+    
+
+    print(list_value)
+
+
+three()
