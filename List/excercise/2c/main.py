274b2c932d78f1f62d09f55854da5008d7fdd7bd
derick-droid <derrickokinda9@gmail.com>
2023-09-15 15:32:44 +0300

    2c

--- List/excercise/2c/main.py
+++ List/excercise/2c/main.py
@@ -2,23 +2,19 @@
 Write a program that generates a list of 20 random numbers between 1 and 100.
 (a) Print the list.
 (b) Print the average of the elements in the list.
+(c) Print the largest and smallest values in the list.
 
 """
 import random
 from random import randint
 
 list_value = []
-sum_value = 0
-value = 0
 
 
 def two():
     for num in range(20):
         new_number = random.randint(1, 100)
         list_value.append(new_number)
-
-    average = sum(list_value) / len(list_value)
-    print(average)
 
 
 two()
