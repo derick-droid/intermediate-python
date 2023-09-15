66bf85cea975002c0c62c0fe268b881e7a465ed1
derick-droid <derrickokinda9@gmail.com>
2023-09-15 16:27:52 +0300

    3b

---
+++ List/excercise/3b/main.py
@@ -0,0 +1,16 @@
+"""
+Start with the list [8,9,10]. Do the following:
+(a) Set the second entry (index 1) to 17
+(b) Add 4, 5, and 6 to the end of the list
+"""
+
+
+def three():
+    list_value = [8, 9, 10]
+
+
+    print(list_value + [4, 5, 6])
+
+
+
+three()
