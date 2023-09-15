90d4de8ddaffb1900baa1587ae2102eb6b5b94d1
derick-droid <derrickokinda9@gmail.com>
2023-09-15 16:38:00 +0300

    complete 3

--- List/excercise/3e/main.py
+++ List/excercise/3e/main.py
@@ -3,15 +3,18 @@ Start with the list [8,9,10]. Do the following:
 (a) Set the second entry (index 1) to 17
 (b) Add 4, 5, and 6 to the end of the list
 (c) Remove the first entry from the list
-
+(d) Sort the list
+(e) Double the list
 """
 
 
 def three():
     list_value = [8, 9, 10]
-    list_value.remove(8)
+    new_list = list_value + [4, 5, 6]
+    new_list.remove(8)
+    new_list.sort()
 
-    print(list_value)
+    print(new_list * 2)
 
 
 three()
--- List/excercise/3f/main.py
+++ List/excercise/3f/main.py
@@ -3,15 +3,23 @@ Start with the list [8,9,10]. Do the following:
 (a) Set the second entry (index 1) to 17
 (b) Add 4, 5, and 6 to the end of the list
 (c) Remove the first entry from the list
-
+(d) Sort the list
+(e) Double the list
+(f) Insert 25 at index 3
 """
 
 
 def three():
     list_value = [8, 9, 10]
-    list_value.remove(8)
+    new_list = list_value + [4, 5, 6]
+    new_list.remove(8)
+    new_list.sort()
+    Third_list = new_list * 2
 
-    print(list_value)
+    Third_list.insert(3, 25)
+
+    print(Third_list)
+
 
 
 three()
