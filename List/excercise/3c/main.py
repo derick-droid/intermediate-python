5523796f7cd42802e2ab5c81f088ad43cf0b53b8
derick-droid <derrickokinda9@gmail.com>
2023-09-15 16:31:57 +0300

    3c

--- List/excercise/3c/main.py
+++ List/excercise/3c/main.py
@@ -2,14 +2,15 @@
 Start with the list [8,9,10]. Do the following:
 (a) Set the second entry (index 1) to 17
 (b) Add 4, 5, and 6 to the end of the list
+(c) Remove the first entry from the list
 """
 
 
 def three():
     list_value = [8, 9, 10]
+    list_value.remove(8)
 
-
-    print(list_value + [4, 5, 6])
+    print(list_value)
 
 
 three()
