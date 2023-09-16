d2ee00d9ec181d3e406f62a4404399a68a36b65b
derick-droid <derrickokinda9@gmail.com>
2023-09-16 21:24:03 +0300

    6a

---
+++ List/excercise/6a/main.py
@@ -0,0 +1,15 @@
+"""
+Create the following lists using a for loop.
+(a) A list consisting of the integers 0 through 49
+"""
+
+
+def six_a():
+    list_value = []
+    for item in range(50):
+        list_value.append(item)
+
+    print(list_value)
+
+
+six_a()
