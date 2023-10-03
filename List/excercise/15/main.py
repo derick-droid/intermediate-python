58ccb07e24d60673e07095a215d97f8087ed5b0c
derick-droid <derrickokinda9@gmail.com>
2023-10-03 15:23:09 +0300

    fifteen

--- List/excercise/15/main.py
+++ List/excercise/15/main.py
@@ -15,3 +15,29 @@ the shifts above.
 
 """
 
+def fifteen():
+    from random import randint
+    message = input("Enter a message: ")
+
+    alphabet = list('abcdefghijklmnopqrstuvwxyz')
+    secret = []
+    for character in message.lower():
+        if character in alphabet:
+            i = randint(0, 26)
+            secret.append(alphabet[i])
+        else:
+            secret.append(character)
+
+    print(''.join(secret))
+    """  b. Write a program to decrypt a string encrypted as above. """
+
+    message = list(message)
+    i = 0
+    secret_rev = []
+    for character in secret:
+        secret_rev.append(message[i])
+        i += 1
+
+    print(''.join(secret_rev))
+
+fifteen()
