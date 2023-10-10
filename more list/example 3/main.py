2e52b8f0f3eaf9da78ded7cabfa8d121144a994d
derick-droid <derrickokinda9@gmail.com>
2023-10-10 20:52:26 +0300

    choices

---
+++ more list/example 3/main.py
@@ -0,0 +1,18 @@
+"""
+
+The choice function also works with strings, picking a random character from a
+string. Here is an example that uses choice to fill the screen with a bunch of random characters.
+
+"""
+
+from random import choices
+from random import choice
+
+
+def choice_string():
+    s = "aqwrtyuioplkjhgfdsaxcvbnm"
+    for i in range(1000):
+        print(choice(s), end="")
+
+
+choice_string()
