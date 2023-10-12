af10a1eb4d2a84add4e5270ff5b1844af3853254
derick-droid <derrickokinda9@gmail.com>
2023-10-12 20:30:57 +0300

    splits

---
+++ more list/example 6/main.py
@@ -0,0 +1,15 @@
+"""
+The split method returns a list of the words of a string. The method assumes that words are sep-
+arated by whitespace, which can be either spaces, tabs or newline characters. Here is an example:
+
+
+"""
+
+
+def splitz():
+    word = "I am Coming home"
+    print(word.split())
+
+
+splitz()
+
