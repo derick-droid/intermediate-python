6ff655caa7ea200d34ae9d7fb03aaa17d895342c
derick-droid <derrickokinda9@gmail.com>
2023-10-15 06:41:49 +0300

    split taking argument

---
+++ more list/split-argumrt/main.py
@@ -0,0 +1,11 @@
+"""
+The split method takes an optional argument that allows it to break the
+string at places other than spaces. Here is an example:
+"""
+
+
+def split_argue():
+    number = "8-3003-233-233-36636-27272-333-222"
+    print(number.split("-"))
+
+split_argue()
