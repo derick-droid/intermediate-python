cdc397562368d9fdf582a1b4e82fd463aa3dd73c
derick-droid <derrickokinda9@gmail.com>
2023-10-18 20:05:02 +0300

    stirip

--- more list/comp 2/main.py
+++ more list/comp 2/main.py
@@ -1,7 +1,7 @@
 def word_list():
     M = ['one ', 'two ', 'three ', 'four ', 'five ', 'six ']
 
-    new_list = [letter[0] for letter in M]
+    new_list = [letter[0] for letter in M if len(letter) >= 3]
 
     print(new_list)
 
---
+++ more list/words/main.py
@@ -0,0 +1,9 @@
+"""
+
+
+"""
+
+M = ['one ', 'two ', 'three ', 'four ', 'five ', 'six ']
+new_list = [letter[0] for letter in M if len(letter) == 3]
+
+print(new_list)
