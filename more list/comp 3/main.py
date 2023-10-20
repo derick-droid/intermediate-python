fe45af71468a7f3f6774820623eb7524964c4600
derick-droid <derrickokinda9@gmail.com>
2023-10-20 19:20:02 +0300

    comprehension

--- more list/comp 3/main.py
+++ more list/comp 3/main.py
@@ -2,9 +2,10 @@
 """
 
 L = []
-M = [ 'one ', 'two ', 'three ', 'four ', 'five ', 'six ']
+M = [ 'one', 'two', 'three', 'four', 'five', 'six']
 for m in M:
     print(m)
-#     if len(m) == 3:
-#         L.append(m)
-# print(L)
+    print(len(m))
+    if len(m) == 3:
+        L.append(m)
+print(L)
