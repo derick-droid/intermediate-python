881c9dfdc8a5e3225a8ae382a7e9841144d5b94d
derick-droid <derrickokinda9@gmail.com>
2023-11-03 20:57:17 +0300

    dimensional

---
+++ more list/dimension 1/main.py
@@ -0,0 +1,16 @@
+"""
+There are a number of common things that can be represented by two-dimensional lists, like a Tic-
+tac-toe board or the pixels on a computer screen. In Python, one way to create a two-dimensional
+list is to create a list whose items are themselves lists. Here is an example:
+
+"""
+
+def dimen():
+    L = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+
+    print(L[1] [2])
+
+
+dimen()
