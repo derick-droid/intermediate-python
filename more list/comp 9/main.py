c201e0f624a8949abf1279066bbd109285b03c34
derick-droid <derrickokinda9@gmail.com>
2023-11-03 16:07:10 +0300

    fix

--- more list/comp 9/main.py
+++ more list/comp 9/main.py
@@ -3,11 +3,13 @@ Another example The join method can often be used with list comprehensions to qu
 up a string. Here we create a string that contains a random assortment of 1000 letters.
 """
 
-from random import  choice
+from random import choice
+
+
 def alpha():
     letter = "abcdefghijklmnopqrstuvwxyz"
 
-    s = "".join([choice(letter) for i in range (1000)])
+    s = "".join([choice(letter) for i in range(1000)])
     print(s)
 
 
