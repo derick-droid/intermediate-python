377d934367e66d1bc85b60266306f1fab1ecc30a
derick-droid <derrickokinda9@gmail.com>
2023-11-06 15:28:44 +0300

    dime

---
+++ more list/dimension 2/main.py
@@ -0,0 +1,17 @@
+"""
+Printing a two-dimensional list To print a two-dimensional list, you can use nested for loops.
+The following example prints a 10 × 5 list:
+
+"""
+
+
+
+def dimen():
+    L = []
+    for r in range(10):
+        for c in range(10):
+            print(L[r][c], end=" ")
+    print()
+
+
+dimen()
