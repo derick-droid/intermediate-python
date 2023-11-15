a209e7dba22fd9668df594eea11cd68aeefa2be2
derick-droid <derrickokinda9@gmail.com>
2023-11-15 09:08:03 +0300

    more list

--- more list/next/main.py
+++ more list/next/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+"""
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
 
+L = [[1, 2, 3],
+  [4, 5, 6],
+[7 ,8 ,9]]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+count = sum([1 for r in range(10) for c in range(5) if L[r][c] % 2 == 0])
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(count)
