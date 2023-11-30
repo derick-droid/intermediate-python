b5dfce56029591727598f43ed99f756d998d23dd
derick-droid <derrickokinda9@gmail.com>
2023-11-30 20:36:37 +0300

    pass

---
+++ more list/exercise/no 5/main.py
@@ -0,0 +1,7 @@
+"""
+Write a simple quote-of-the-day program. The program should contain a list of quotes, and
+when the user runs the program, a randomly selected quote should be printed.
+"""
+
+def quote():
+    
