29b478121c2959946a14c49e2d7867bc313904b3
derick-droid <derrickokinda9@gmail.com>
2024-01-11 12:10:47 +0300

    fix sample

--- sample/main.py
+++ sample/main.py
@@ -1,1 +1,27 @@
 import random
+
+
+def lottery():
+    # Step 1: Initialize variables and constants
+
+    number_range = (range, 111)
+
+    total_drawings = 0
+
+    for time in range(1000):
+        user_number = set(random.sample(number_range, 6))
+        drawn_numbers = set()
+        drawings = 0
+
+    
+
+
+
+
+
+
+
+
+
+
+
