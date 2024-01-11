376f5344e315ee30a0be2d89aab6f62db287e344
derick-droid <derrickokinda9@gmail.com>
2024-01-11 16:14:34 +0300

    by pass

--- more list/exercise/no 8/main.py
+++ more list/exercise/no 8/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+"""
+Write a program that simulates drawing names out of a hat. In this drawing, the number of
+hat entries each person gets may vary. Allow the user to input a list of names and a list of
+how many entries each person has in the drawing, and print out who wins the drawing.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+"""
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
