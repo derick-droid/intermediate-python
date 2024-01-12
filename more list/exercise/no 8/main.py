521b67acaee5a0d619588cb9b4eee8e02f9676dc
derick-droid <derrickokinda9@gmail.com>
2024-01-12 11:05:35 +0300

    mux

--- more list/exercise/no 8/main.py
+++ more list/exercise/no 8/main.py
@@ -5,3 +5,28 @@ how many entries each person has in the drawing, and print out who wins the draw
 
 """
 
+import random
+
+
+def hat_lottery():
+    hat = []
+
+    for name in range(10):
+        names = input("Enter name: ")
+
+        hat.append(names)
+
+    right_choice = random.choices(hat)
+
+    random.shuffle(hat)
+
+    computer_choice = random.choices(hat)
+
+    if right_choice == computer_choice:
+        print(f"yes you have won the answer is {computer_choice}")
+
+    else:
+        print(f" No {right_choice} is not the answer")
+
+
+hat_lottery()
