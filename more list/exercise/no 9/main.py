16ca72cbc4e149086e536dd5ffdaa310a350287a
derick-droid <derrickokinda9@gmail.com>
2024-01-19 10:31:12 +0300

    fixed nine

---
+++ more list/exercise/no 9/main.py
@@ -0,0 +1,6 @@
+"""
+Write a simple quiz game that has a list of ten questions and a list of answers to those ques-
+tions. The game should give the player four randomly selected questions to answer. It should
+ask the questions one-by-one, and tell the player whether they got the question right or
+wrong. At the end it should print out how many out of four they got right.
+"""
