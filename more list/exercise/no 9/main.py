cd13c888195a1c9e4f521ca68cf3a5e8b62df1a6
derick-droid <derrickokinda9@gmail.com>
2024-01-24 15:30:59 +0300

    tkinter

--- more list/exercise/no 9/main.py
+++ more list/exercise/no 9/main.py
@@ -3,4 +3,23 @@ Write a simple quiz game that has a list of ten questions and a list of answers 
 tions. The game should give the player four randomly selected questions to answer. It should
 ask the questions one-by-one, and tell the player whether they got the question right or
 wrong. At the end it should print out how many out of four they got right.
+
 """
+
+def list_quiz():
+    list_question = ["What is the capital city of Kenya? ",
+                     "Who is the current president of Tanzania? ",
+                     "Name the tallest Mount in Africa? ",
+                     "Longest River in Africa: ",
+                     "The Current president of US: "
+                     ]
+
+    list_answer = [
+        "Nairobi",
+        "Samia Suluhu",
+        "Kilimanjaro",
+        "Nile",
+        "Biden"
+    ]
+
+    
